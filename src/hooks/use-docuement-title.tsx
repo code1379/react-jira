24aeff556ab88c477c96dce21e986d228fdb9ddf
code1379 <coderxialuo@gmail.com>
2021-06-03 23:03:12 +0800

    fix: 使用useRef优化useDocumentTitle

--- src/hooks/use-docuement-title.tsx
+++ src/hooks/use-docuement-title.tsx
@@ -1,11 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 // 在卸载的时候是否保留？true 为保留， false 为不保留，替换为之前的 title
 export const useDocumentTitle = (
   title: string,
   keepOnUmount: boolean = true
 ) => {
-  const oldTitle = document.title;
+  const oldTitle = useRef(document.title).current;
   console.log("渲染时的oldTitle：", oldTitle);
   useEffect(() => {
     document.title = title;
@@ -19,5 +19,5 @@ export const useDocumentTitle = (
         document.title = oldTitle;
       }
     };
-  }, []);
+  }, [keepOnUmount, oldTitle]);
 };
